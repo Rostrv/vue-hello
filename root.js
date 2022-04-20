d1d901f8fae5edaf788a17e0370ad103c5ea1a09
Rostrv <trovatorosario91@gmail.com>
2022-04-20 13:48:58 +0200

    exercise + bonus

---
+++ root.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+    // option object
+    el: '#app',
+    //data object
+    data: {
+        greetings: 'Hello VueJS',
+        image: ['https://www.cinematographe.it/wp-content/uploads/2015/11/Uss-Enterprise-728x409.jpg']
+
+    }
+})
